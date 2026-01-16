58f975d7bfd1fded38b563677a882e93d80ae79a
dvrkdev <dvrk.devx@gmail.com>
2026-01-16 17:58:54 +0500

    Refactor URL routing and implement class-based views for product listing and detail

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_404
 from django.views.generic import ListView, DetailView
 from .models import Product, Category
 
 
-# function based view
-def product_list(request):
-    products = Product.objects.filter(is_active=True)
-    return render(request, "myapp/product_list.html", {"products": products})
+# # function based view
+# def product_list(request):
+#     products = Product.objects.filter(is_active=True)
+#     return render(request, "myapp/product_list.html", {"products": products})
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "myapp/product_list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "myapp/product_detail.html"
+    context_object_name = "product"
