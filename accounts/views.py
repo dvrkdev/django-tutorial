1b86a178243a67323d32c9361d708e72f67dd1d7
dvrkdev <dvrk.devx@gmail.com>
2026-01-22 10:31:34 +0500

    Default django form and register user view created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            print(type(user))
+            print(user)
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register_user.html', {'form': form})
