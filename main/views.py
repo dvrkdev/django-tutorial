ad80a35610065faf3ee819d40a3a14da796b209c
dvrkdev <dvrk.devx@gmail.com>
2026-01-24 09:22:54 +0500

    Templates moved to root directory

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from accounts.models import CustomUser
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = CustomUser
     template_name = "main/home.html"
+    context_object_name = 'users'
+    paginate_by = 10
