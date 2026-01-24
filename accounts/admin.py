4609e997aba38f71a62aa112cf817cfc4a9a3877
dvrkdev <dvrk.devx@gmail.com>
2026-01-24 08:55:24 +0500

    Admin panel configured

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "location", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Social Links & Info",
+            {
+                "fields": (
+                    "location",
+                    "bio",
+                    "github_link",
+                    "linkedin_link",
+                    "x_link",
+                    "website_link",
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Social Links & Info",
+            {
+                "fields": (
+                    "location",
+                    "bio",
+                    "github_link",
+                    "linkedin_link",
+                    "x_link",
+                    "website_link",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
