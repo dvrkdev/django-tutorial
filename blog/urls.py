0fdbc5d1fc391a087fa956c8273c9a1389a95126
dvrkdev <dvrk.devx@gmail.com>
2026-01-26 12:03:26 +0500

    Templates created

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # home view
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+posts: list[dict] = [
+    {
+        "author": "John Doe",
+        "title": "Blog Post 1",
+        "content": "First post content!",
+        "date_posted": "Jun 24, 2025",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content!",
+        "date_posted": "Jul 10, 2025",
+    },
+]
+
+
+def home(request) -> HttpResponse:
+    return render(request, "blog/home.html", {"posts": posts})
+
 
 # about view
-def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+def about(request) -> HttpResponse:
+    return render(request, 'blog/about.html')
